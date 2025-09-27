a3116f046a1f4d5c33c8707e40a08aed90b58223
Dylan Faulkner <dylanfaulkner616@gmail.com>
2025-09-27 19:40:50 +0000

    Bugfix: issue where multiple instances could be spam opened on top of each other, this would overflow the buffer

--- FileNav.lua
+++ FileNav.lua
@@ -1,27 +1,26 @@
 -- vim = vim.api
-vim = vim
-local os = jit.os 
+local vim = vim
+local os = jit.os
+local common_files = {}
 
 -- Windows, Darwin(MAC), Linux
-if os == "Linux" then 
+if os == "Linux" then
 	common_files = {
-		{name = "FileNav", path = ""},
-		{name = "=======", path = ""},
-		{name = "Home", path = "~/"},
-		{name = "University", path = "~/University"},
-		{name = "Nvim", path = "~/.config/nvim"},
-		{name = "Projects", path = ""},
+		{ name = "Home",       path = "~/" },
+		{ name = "University", path = "~/University" },
+		{ name = "Nvim",       path = "~/.config/nvim" },
+		{ name = "Projects",   path = "~/.config/nvim/lua/custom/" },
 	}
 end
 
 -- Count number of common locations -> Will use info to inform window size
-counter = 0
+local counter = 0
 for i, file in ipairs(common_files) do
 	counter = counter + 1
 end
 
 -- Count number of largest name -> Will also use for window size
-largestName = 0
+local largestName = 0
 for i, file in ipairs(common_files) do
 	if #file.name > largestName then
 		largestName = #file.name
@@ -29,10 +28,16 @@ for i, file in ipairs(common_files) do
 end
 
 local lastLocation = ""
+local buf = vim.fn.bufnr("NavMenu")
 
 local function openWindow()
+
+	if buf == -1 and not vim.api.nvim_buf_is_valid(buf) then
+		buf = vim.api.nvim_create_buf(false, true)
+		vim.api.nvim_buf_set_name(buf, "NavMenu")
+	end
+
 	-- Create a new buffer and window
-	local buf = vim.api.nvim_create_buf(false, true) -- create new empty buffer
 	local win = vim.api.nvim_open_win(buf, true, {
 		relative = 'win',
 		width = largestName + 2,
@@ -50,11 +55,8 @@ local function openWindow()
 	end
 
 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines) -- populates the buffer with my array of locations
-	vim.api.nvim_buf_set_name(buf, "NavMenu")       -- set name for buffer
 
-	vim.keymap.set("n", "q", function()
-		vim.cmd("bwipeout! " .. buf) -- Neat function from the docs -> Wipes a buffer with params
-	end, { buffer = buf, silent = true })
+	vim.api.nvim_buf_set_keymap(buf, "n", "q", ":close<CR>", { noremap = true, silent = true })
 
 	vim.keymap.set("n", "<CR>", function()
 		local cursor = vim.api.nvim_win_get_cursor(0)
@@ -63,7 +65,7 @@ local function openWindow()
 		for i, file in ipairs(common_files) do
 			if file.name == choice then
 				if file.path == "" or file.path == " " then
-					return 
+					return
 				end
 				print(file.path)
 				lastLocation = vim.fn.getcwd()
@@ -77,8 +79,7 @@ local function openWindow()
 				-- table.insert(common_files, {name = "Last Location", path = lastLocation})
 			end
 		end
-	end, {noremap = true, silent = true})
-
+	end, { noremap = true, silent = true })
 end
 
 -- create a command to open the window
@@ -87,4 +88,11 @@ vim.api.nvim_create_user_command('NavMenu', function()
 end, { desc = "Opens a navigation menu" })
 
 -- Mapping to open
-vim.keymap.set("n", "<C-l>", ":NavMenu<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", function()
+	-- ":NavMenu<CR>"
+	if vim.fn.bufwinnr(buf) == -1 then
+		vim.cmd("NavMenu")
+	else
+		print("Nav already open")
+	end
+end, { noremap = true, silent = true })
--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 A simple neovim script to create a navigation menu for quick access to frequently used files and directories.
 Currently a WIP.
 
+##  Current Issues:
+- Issue with being able to create multiple navigators all on top of each other.
+
 ### Possible enhancements:
+- Add ability to use during file navigation -> Currently have to have an active buffer to bind it to, so cannot use it in explorer.
 - Add previous location in order to return.
 - Sort spacing and alignment.
 - Add icons.
